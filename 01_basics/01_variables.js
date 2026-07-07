6a4e8ae26f40ae79609a33c3d1890889c1085fa5
Vikrant3456 <vikrantpatel2702@gmail.com>
2026-07-07 23:38:41 +0000

    learned about variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId=144553
+let accountEmail="vikrant@gmail.com"
+var accountPassword="vikrant@123"
+accountCity="Jaipur"
+let accountState
+
+//accountId=2
+accountEmail="vikrant@123"
+accountPassword="vikrant@1234"
+accountCity="Delhi"
+/*console.log(accountEmail)
+console.log(accountPassword)
+console.log(accountCity)*/
+console.table({accountId,accountEmail,accountPassword,accountCity,accountState
+})
