3c98de8043cbe2e7b9b716985754d1665e7f2f1c
Vikrant3456 <vikrantpatel2702@gmail.com>
2026-07-08 16:50:04 +0000

    comparison

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,13 @@
+// console.log("2">1)
+// console.log("02">1)
+
+// console.log(null>0) // false
+// console.log(null==0) // false
+// console.log(null>=0) // true
+
+console.log(undefined==null) // true
+console.log(undefined===null) // false
+
+// === strict check also checks datatype
+console.log("2"==2) // true
+console.log("2"===2) // false
