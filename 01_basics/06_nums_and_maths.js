7ff9d926042c6613781e7284a5da20cc635ddd1a
Vikrant3456 <vikrantpatel2702@gmail.com>
2026-07-08 20:57:13 +0000

    nums and maths

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,30 @@
+const score = 100
+//console.log(score)
+const balance = new Number(1000)
+// console.log(balance)
+// console.log(balance.valueOf())
+// console.log(balance.toFixed(4))
+
+const otherNumber = 123.456789
+// console.log(otherNumber.toPrecision(4))
+// console.log(otherNumber.toPrecision(6))
+// console.log(otherNumber.toPrecision(2))
+
+const hundreds=1000000
+// console.log(hundreds.toLocaleString('en-IN'))
+
+// ********************maths*******************************
+
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.4))
+// console.log(Math.floor(4.9))
+// console.log(Math.ceil(4.1))
+// console.log(Math.min(0, 150, 30, 20, -8, -200))
+// console.log(Math.max(0, 150, 30, 20, -8, -200))
+console.log(Math.random())
+
+const min=10
+const max=20
+const randomNumber=Math.floor(Math.random()*(max-min)+min)
+console.log(randomNumber)
