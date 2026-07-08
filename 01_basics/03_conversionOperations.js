b471cdb9fdb3c99a00c86ec7396437a6e5e765b0
Vikrant3456 <vikrantpatel2702@gmail.com>
2026-07-08 12:59:53 +0000

    conversionOperation

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,38 @@
+let score=true
+
+//console.log(typeof score)
+//console.log(typeof (score))
+
+let valueInNumber=Number(score)
+
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
+
+let isLoggedIn="76"
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1-> true
+// 0-> false
+// "vikrant"-> true
+// ""-> false
+// 54-> true
+
+let num=34
+let stringNumber=String(num)
+//console.log(stringNumber)
+//console.log(typeof stringNumber)
+
+let value=3
+let negValue=-value
+console.log(negValue)
+console.log(2**3)
+
+let str1="Hello"
+let str2=" Vikrant"
+let str3=str1+str2
+console.log(str3)
+console.log(1+2+"2")
+console.log("1"+2+2)
+console.log((typeof (1+2+"2")))
+console.log((typeof ("1"+2+2)))
