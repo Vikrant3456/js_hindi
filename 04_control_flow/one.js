e1034888769fef20efadc621fe7647477f3518de
Vikrant3456 <vikrantpatel2702@gmail.com>
2026-07-09 21:33:34 +0000

    if else,switch,truthy falsy

---
+++ 04_control_flow/one.js
@@ -0,0 +1,43 @@
+//if
+const isUserLoggedIn=true
+const Temperature=41
+/*if(Temperature<50){
+    console.log("Temperature is less than 50")
+}
+else{
+    console.log("Temperature is not less than 50")
+}*/
+
+const score=200
+
+if(score>100){
+    const power="fly"
+    //console.log(`Use power: ${power}`)
+}
+//console.log(`Use power: ${power}`)
+
+const balance=1000
+/*if(balance>500){
+    console.log("test");
+}*/
+/*
+if(balance<500){
+    console.log("less than 500")
+}
+else if(balance<750){
+    console.log("less than 750")
+}
+else{
+    console.log("less than 1200")
+}*/
+
+// const userLoggedIn=true
+// const debitCard=true
+// const loggedInFromGoogle=false
+// const loggedInFromEmail=true
+// if(userLoggedIn && debitCard){
+//     console.log("Allow to buy course")
+// }
+// if(loggedInFromGoogle || loggedInFromEmail){
+//     console.log("User logged in")
+// }
---
+++ 04_control_flow/switch.js
@@ -0,0 +1,27 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month="feb"
+
+switch (month) {
+    case "jan":
+        console.log("January")
+        break;
+
+    case "feb":
+        console.log("February")
+        break;
+    case "mar":
+        console.log("March")
+        break;
+
+    default:
+        console.log("Default case match")
+        break;
+}
