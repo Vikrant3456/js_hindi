63630f308c6b72a859499e6f7e2b9cc2ccf6c046
Vikrant3456 <vikrantpatel2702@gmail.com>
2026-07-12 22:12:13 +0000

    getter setter

---
+++ 10_classes_and_oop/getter_setter.js
@@ -0,0 +1,23 @@
+class User{
+    constructor(email,password){
+        this.email=email,
+        this.password=password
+    }
+
+    get email(){
+        return this._email.toUpperCase()
+    }
+    set email(value){
+        this._email=value
+    }
+
+    get password(){
+        return this._password.toUpperCase()
+    }
+    set password(value){
+        this._password=value
+    }
+}
+
+const vikrant = new User("virkant@gmail.com","abc")
+console.log(vikrant.email)
